64bb5a8abf33ee807547af17975d3f60a0a4f7ed
Gregory Nelson Esguerra <gregory_keyboard@outlook.com>
2024-02-06 16:02:55 +0800

    Add project files.

---
+++ NullParameterChecking/Program.cs
@@ -0,0 +1,17 @@
+﻿namespace NullParameterChecking
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            string? email = null;
+            CheckEmail(email);
+        }
+
+        static void CheckEmail(string email)
+        {
+            ArgumentNullException.ThrowIfNull(email);
+            Console.WriteLine(email);
+        }
+    }
+}
